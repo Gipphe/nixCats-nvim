eb034c63c229ec3d962ca903269459c2e46377ea
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-07-27 12:42:58 +0200

    chore: add cachix cache

--- flake.nix
+++ flake.nix
@@ -1,4 +1,12 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     wrappers = {
